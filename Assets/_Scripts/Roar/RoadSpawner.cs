f2ede7eb7ad9033392f589a3217db026a0478dec
FunGi2410 <nguyennhanit2410@gmail.com>
2025-03-25 20:17:04 +0700

    static vehicle obstacle spawner

--- Assets/_Scripts/Roar/RoadSpawner.cs
+++ Assets/_Scripts/Roar/RoadSpawner.cs
@@ -4,25 +4,25 @@ public class RoadSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] roadPrefabs;
     Vector3 nextSpawnPoint;
-    //VehicleSpawner vehicleSpawner;
+    VehicleSpawner vehicleSpawner;
 
     private void Awake()
     {
-        //this.vehicleSpawner = GameObject.FindGameObjectWithTag("VehicleSpawner").GetComponent<VehicleSpawner>();
+        vehicleSpawner = GameObject.FindGameObjectWithTag("VehicleSpawner").GetComponent<VehicleSpawner>();
     }
     private void Start()
     {
-        this.Spawn();
-        this.Spawn();
+        Spawn();
+        Spawn();
     }
 
     public void Spawn()
     {
-        int index = Random.Range(0, this.roadPrefabs.Length);
-        GameObject tile = Instantiate(this.roadPrefabs[index], this.nextSpawnPoint, Quaternion.identity);
+        int index = Random.Range(0, roadPrefabs.Length);
+        GameObject tile = Instantiate(roadPrefabs[index], nextSpawnPoint, Quaternion.identity);
         // Vehicle Spawn
-        //this.vehicleSpawner.Spawn(this.nextSpawnPoint);
-        this.nextSpawnPoint = tile.transform.GetChild(0).transform.position;
+        vehicleSpawner.Spawn(nextSpawnPoint);
+        nextSpawnPoint = tile.transform.GetChild(0).transform.position;
 
         
     }
